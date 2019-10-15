6ffd4f7178097bcca64ee061a42790f8da382e3a
Evan Neiswender <esn002@lvc.edu>
2019-10-14 20:00:51 -0400

    --

--- src/NodeHomework.java
+++ src/NodeHomework.java
@@ -1,7 +1,13 @@
 public class NodeHomework {
 
     public class Node<T>{
+        public Object idk;
         public Node() {}
-        
+
+    }
+
+
+    public static void main(String[] args){
+
     }
 }
