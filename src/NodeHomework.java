75623a5be0ff79f988864aebf82ca5af4e4b49a4
Evan Neiswender <esn002@lvc.edu>
2019-10-15 15:14:31 -0400

    Wrote NodeHomework - want to make sure its right

--- src/NodeHomework.java
+++ src/NodeHomework.java
@@ -1,12 +1,25 @@
 public class NodeHomework {
 
     public class Node<T>{
-        public T element;
-        public Node(T t, String name) {
+        private T element;
+        private Node next;
+        public Node(T t, Node next) {
             this.element = t;
-            System.out.println(name);
+            next = null;
+
         }
 
+        public T getElement(Node n){return element;}
+
+        public void setElement(Node n, T element){
+            n.element = element;
+        }
+
+        public Node getNext(Node n){return n.next;}
+
+        public void setNext(Node n, Node next){
+            n.next = next;
+        }
     }
 
 
